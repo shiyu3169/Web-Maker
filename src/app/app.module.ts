fd5a87037e7b0b7df39826155faff6b80f825c30
Shiyu Wang <shiyu3169@gmail.com>
2018-10-29 14:16:27 -0400

    routing & user, website routerlinks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { Routing } from "./app.routing";
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './components/user/login/login.component';
-import { RegisterComponent } from './components/user/register/register.component';
-import { ProfileComponent } from './components/user/profile/profile.component';
-import { WebsiteListComponent } from './components/website/website-list/website-list.component';
-import { WebsiteNewComponent } from './components/website/website-new/website-new.component';
-import { WebsiteEditComponent } from './components/website/website-edit/website-edit.component';
-import { PageListComponent } from './components/page/page-list/page-list.component';
-import { PageNewComponent } from './components/page/page-new/page-new.component';
-import { PageEditComponent } from './components/page/page-edit/page-edit.component';
-import { WidgetListComponent } from './components/widget/widget-list/widget-list.component';
-import { WidgetChooserComponent } from './components/widget/widget-chooser/widget-chooser.component';
-import { WidgetEditComponent } from './components/widget/widget-edit/widget-edit.component';
-import { WidgetHeadingComponent } from './components/widget/widget-edit/widget-heading/widget-heading.component';
-import { WidgetImageComponent } from './components/widget/widget-edit/widget-image/widget-image.component';
-import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-youtube/widget-youtube.component';
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/user/login/login.component";
+import { RegisterComponent } from "./components/user/register/register.component";
+import { ProfileComponent } from "./components/user/profile/profile.component";
+import { WebsiteListComponent } from "./components/website/website-list/website-list.component";
+import { WebsiteNewComponent } from "./components/website/website-new/website-new.component";
+import { WebsiteEditComponent } from "./components/website/website-edit/website-edit.component";
+import { PageListComponent } from "./components/page/page-list/page-list.component";
+import { PageNewComponent } from "./components/page/page-new/page-new.component";
+import { PageEditComponent } from "./components/page/page-edit/page-edit.component";
+import { WidgetListComponent } from "./components/widget/widget-list/widget-list.component";
+import { WidgetChooserComponent } from "./components/widget/widget-chooser/widget-chooser.component";
+import { WidgetEditComponent } from "./components/widget/widget-edit/widget-edit.component";
+import { WidgetHeadingComponent } from "./components/widget/widget-edit/widget-heading/widget-heading.component";
+import { WidgetImageComponent } from "./components/widget/widget-edit/widget-image/widget-image.component";
+import { WidgetYoutubeComponent } from "./components/widget/widget-edit/widget-youtube/widget-youtube.component";
 
 @NgModule({
   declarations: [
@@ -37,10 +38,8 @@ import { WidgetYoutubeComponent } from './components/widget/widget-edit/widget-y
     WidgetImageComponent,
     WidgetYoutubeComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, Routing],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
