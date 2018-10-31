1337ee7346878a54086f7302f8185ae9b696dad7
Shiyu Wang <shiyu3169@gmail.com>
2018-10-31 09:25:54 -0400

    data services done

---
+++ src/app/services/widget.service.client.ts
@@ -0,0 +1,75 @@
+import { Injectable } from "@angular/core";
+
+// injecting service into module
+@Injectable()
+export class PageService {
+  widgets = [
+    {
+      _id: "123",
+      widgetType: "HEADING",
+      pageId: "321",
+      size: 2,
+      text: "GIZMODO"
+    },
+    {
+      _id: "234",
+      widgetType: "HEADING",
+      pageId: "321",
+      size: 4,
+      text: "Lorem ipsum"
+    },
+    {
+      _id: "345",
+      widgetType: "IMAGE",
+      pageId: "321",
+      width: "100%",
+      url: "http://lorempixel.com/400/200/"
+    },
+    {
+      _id: "567",
+      widgetType: "HEADING",
+      pageId: "321",
+      size: 4,
+      text: "Lorem ipsum"
+    },
+    {
+      _id: "678",
+      widgetType: "YOUTUBE",
+      pageId: "321",
+      width: "100%",
+      url: "https://youtu.be/AM2Ivdi9c4E"
+    }
+  ];
+
+  createWidget(widget) {
+    widget._id = Math.random.toString();
+    this.widgets.push(widget);
+    return widget;
+  }
+  findWidgetsByPageId(pageId) {
+    let result = [];
+    for (let i = 0; i < this.widgets.length; i++) {
+      if (pageId === this.widgets[i].pageId) {
+        result.push(this.widgets[i]);
+      }
+    }
+    return result;
+  }
+  findWidgetById(widgetId) {
+    for (let i = 0; i < this.widgets.length; i++) {
+      if ((widgetId = this.widgets[i]._id)) {
+        return this.widgets[i];
+      }
+    }
+  }
+  updateWidget(widget) {
+    const oldWidget = this.findWidgetById(widget._id);
+    const index = this.widgets.indexOf(oldWidget);
+    this.widgets[index] = widget;
+  }
+  deleteWidget(widgetId) {
+    const oldWidget = this.findWidgetById(widgetId);
+    const index = this.widgets.indexOf(oldWidget);
+    this.widgets.splice(index, 1);
+  }
+}
